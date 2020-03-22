48be25d25887c467820bdfaf910ac9eae6647819
田淵健太 <>
2020-03-22 23:49:58 +0900

    見た目の雰囲気を整えるまで。

---
+++ resources/views/main.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="css/windows.css">
+  <link rel="stylesheet" type="text/css" href="css/text.css">
+  <link rel="stylesheet" type="text/css" href="css/reset.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="win-backscreen">
+    <div class="win-wallet">
+      <p class="text-wallet">¥</p>
+    </div>
+    <div class="win-enemyimage">
+      <img src="image/fantasy_maou_devil.png">
+    </div>
+    <div class="win-command">
+      <ul>
+        <li class="text-command">買い向かう</li>
+        <li class="text-command">空売り</li>
+        <li class="text-command">様子を見る</li>
+        <li class="text-command">撤退</li>
+      </ul>
+    </div>
+    <div class="win-message">
+    </div>
+  </div>
+</body>
+</html>
